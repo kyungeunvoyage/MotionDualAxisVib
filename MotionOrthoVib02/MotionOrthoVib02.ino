381ed5074b1b63bb23b293221105c650561a32c9
Kyungeun Jung <86193432+kyungeunvoyage@users.noreply.github.com>
2025-08-03 11:44:34 +0900

    Add MotionOrthoVib03 and update shared Haptic_Driver files

--- MotionOrthoVib02/MotionOrthoVib02.ino
+++ MotionOrthoVib02/MotionOrthoVib02.ino
@@ -1,26 +1,64 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       MotionOrthoVib02.ino
-    Created:	2025-08-03 오전 11:06:19
-    Author:     DESKTOP-PADEJ5U\HCITECH_01
-*/
+#include <Wire.h>
+#include "Haptic_Driver.h"
 
-// Define User Types below here or use a .h file
-//
+//=========================DA7280============================
+#define NUMBER_OF_SENSORS 2
+Haptic_Driver hapDrive[NUMBER_OF_SENSORS];
+hapticSettings hf;
+#define MUX_ADDR 0x70
+bool daInitialized[NUMBER_OF_SENSORS];
+
+int burst75ms = 75;
+int burst200ms = 200;
+int burst500ms = 500;
+
+float currentFreq = 10.0;
+bool vibrating = false;
+unsigned long vibStart = 0;
+unsigned long vibDuration = 0;
+//================DA7280====================
+
+//================setup the MUX and DA7280====================
+void TCA9548A(uint8_t bus) {
+    Wire.beginTransmission(0x70);
+    Wire.write(1 << bus);
+    Wire.endTransmission();
+}
+
+void DA7280setup()
+{
+    hf.motorType = LRA_TYPE;
+    hf.absVolt = 3.5;
+    hf.nomVolt = 2.47;
+    hf.currMax = 295.1;
+    hf.impedance = 8.37;
+    //init frequency 
+    hf.lraFreq = 5;
+    Wire.begin();
+
+    for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
+        TCA9548A(i);
+        hapDrive[i].begin();
+        daInitialized[i] = true;
+        if (!hapDrive[i].begin()) {
+            Serial.print("Could not communicate with Haptic Driver ");
+            Serial.println(i);
+        }
+        hapDrive[i].enableFreqTrack(false);
+        hapDrive[i].setOperationMode(DRO_MODE);
+    }
+}
+//================setup the MUX and DA7280====================
 
 
-// Define Function Prototypes that use User Types below here or use a .h file
-//
 
-
-// Define Functions below here or use other .ino or cpp files
-//
 
 // The setup() function runs once each time the micro-controller starts
 void setup()
 {
-
+    Serial.begin(115200);
+    Serial.println("Hello Hello");
+    DA7280setup();
 
 }
 
---
+++ MotionOrthoVib03/MotionOrthoVib03.ino
@@ -0,0 +1,19 @@
+﻿/*
+ Name:		MotionOrthoVib03.ino
+ Created:	2025-08-03 오전 11:41:25
+ Author:	HCITECH_01
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <Haptic_DA7280.h>
+#include <Haptic_DA7280_registers.h>
+
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
