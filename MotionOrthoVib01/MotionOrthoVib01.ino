36885ac6ed060952fab3fe55205a6df80e6cdda2
Kyungeun Jung <86193432+kyungeunvoyage@users.noreply.github.com>
2025-08-03 11:12:17 +0900

    Initial commit

---
+++ MotionOrthoVib01/MotionOrthoVib01.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       MotionOrthoVib01.ino
+    Created:	2025-08-03 오전 11:06:03
+    Author:     DESKTOP-PADEJ5U\HCITECH_01
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
