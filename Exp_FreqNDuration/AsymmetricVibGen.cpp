213c6a339c2833cdb7dcece5a8fe0f86d6cb8f83
Kyungeun Jung <86193432+kyungeunvoyage@users.noreply.github.com>
2025-10-01 15:29:15 +0900

    update the frenduration module

---
+++ Exp_FreqNDuration/AsymmetricVibGen.cpp
@@ -0,0 +1,7 @@
+// 
+// 
+// 
+
+#include "AsymmetricVibGen.h"
+
+
---
+++ Exp_FreqNDuration/MPUsetting.h.h
@@ -0,0 +1,24 @@
+// MPUsetting.h.h
+
+#ifndef _MPUSETTING.H_h
+#define _MPUSETTING.H_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class MPUsetting.hClass
+{
+ protected:
+
+
+ public:
+	void init();
+};
+
+extern MPUsetting.hClass MPUsetting.h;
+
+#endif
+
